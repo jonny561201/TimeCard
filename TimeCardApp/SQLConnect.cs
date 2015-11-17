aaad88ff1b682e24b8bbdf1c9a1c017d3d710774
Jon Graf <jongraf1@gmail.com>
2015-11-16 20:43:45 -0600

    Fixed Database Read Method  to return a Dictionary of all items.

--- TimeCardApp/SQLConnect.cs
+++ TimeCardApp/SQLConnect.cs
@@ -6,9 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TimeCardApp
+namespace TimeCardApp   
 {
-    class SqlConnect
+    class SqlConnect : ISQLInterface
     {
         SqlConnection testConn = new SqlConnection("user id=Inuyasha-PC\\Inuyasha;pwd=thexfiles1!;server=INUYASHA-PC\\SQLEXPRESS;Trusted_Connection=yes;database=Work Database;connect timeout=30");
 //        SqlCommand testCommand = new SqlCommand();
@@ -25,7 +25,7 @@ namespace TimeCardApp
             return testConn;
         }
 
-        internal void WriteTable(String InsertValue)
+        public void WriteTable(String InsertValue)
         {
             SqlCommand writeCmd = new SqlCommand();
             writeCmd.Connection = testConn;
@@ -33,9 +33,9 @@ namespace TimeCardApp
             writeCmd.ExecuteNonQuery();
         }
 
-        internal String ReadTable(String ReadValue)
+        public String ReadTable(String ReadValue)
         {
-            SqlCommand ReadCmd = new SqlCommand();
+            SqlCommand ReadCmd = new SqlCommand("select * from ");
             Sql
             ReadCmd.CommandText = ReadValue;
             ReadCmd.Connection = testConn;
---
+++ TimeCardApp/SQLInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCardApp
+{
+    interface ISQLInterface
+    {
+        SqlConnection SqlConnector();
+        SqlConnection SqlClose();
+        void WriteTable(String InsertValue);
+        String ReadTable(String ReadValue);
+
+    }
+}
